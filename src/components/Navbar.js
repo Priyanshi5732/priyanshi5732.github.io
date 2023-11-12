e39b39c49407e5f9821ce919ba3a7ae7cf1e61ad
Priyanshi <gs2113012@sgsitsindore.in>
2023-11-12 10:48:59 +0530

    Added react website

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+
+function Navbar() {
+ 
+  return (
+    <div className="navbar">
+      <div className="leftSide">
+        <p className="tommy">
+        <div><h2>my-space</h2>
+        </div>
+        </p>
+        <div className="hiddenLinks">
+          <Link to="/About"> About </Link>
+          <Link to="/Hackathons"> Hackathons </Link>
+          <Link to="/Projects"> Projects </Link>
+        </div>
+      </div>
+      <div className="rightSide">
+        <Link to="/About"> About </Link>
+        <Link to="/Hackathons"> Hackathons </Link>
+        <Link to="/Projects"> Projects </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
