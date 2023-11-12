5828b72e25155528d71b8b781e9a9f68277a6a5d
Priyanshi <gs2113012@sgsitsindore.in>
2023-11-12 10:39:42 +0530

    Added new react code

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/About.css";
+function About() {
+  return (
+    <div className="about">
+      <div className="aboutBottom">
+        <h1> ABOUT ME</h1>
+        <p>
+          Hi there,my name is Priyanshi Verma
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
