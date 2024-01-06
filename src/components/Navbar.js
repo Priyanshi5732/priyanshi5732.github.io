ac2c68165f4a1dfd2d896b08a1f7fc36741994a4
Priyanshi <gs2113012@sgsitsindore.in>
2024-01-06 23:18:52 +0530

    modified navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 const BasicExample = () => {
   return (
-    <Navbar bg="dark"  expand="lg" className="bg-body-tertiary">
+    <Navbar bg="dark" variant="dark"  expand="lg" >
       <Container fluid>
         <Navbar.Brand href="#">
           <div className="lefttext">
@@ -20,11 +20,11 @@ const BasicExample = () => {
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
-            className="me-auto my-2 my-lg-0"
+            className="rightSide"
             style={{ maxHeight: "100px" }}
             navbarScroll
           >
-            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="Home">Home</Nav.Link>
             <NavDropdown title="Dropdown" id="navbarScrollingDropdown">
               <NavDropdown.Item href="About">About</NavDropdown.Item>
               <NavDropdown.Item href="Hackathons">
