49f4cc340e2d36a37b3e62f69726677a59aaa27d
Priyanshi <gs2113012@sgsitsindore.in>
2024-01-09 23:03:40 +0530

    modified

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import "../styles/Projects.css";
 export default class Projects extends Component{
     render(){
         return (
-        <div className='projects'>Projects</div>
+        <div className='projects'><h1>Projects</h1></div>
         )
     }
 }
